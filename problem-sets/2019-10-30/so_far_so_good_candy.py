69a46fc6acc6db4a06c87580bb18482c2e171d50
Jordan <leejchris21@gmail.com>
2019-10-30 20:15:51 -0700

    candy mountain
    Signed-off-by: Jordan <leejchris21@gmail.com>

--- problem-sets/2019-10-30/so_far_so_good_candy.py
+++ problem-sets/2019-10-30/so_far_so_good_candy.py
@@ -3,21 +3,35 @@ for line in fileinput.input():
     s = line
 
 arr = list(map(int, s))
-i = 0
 total_candy = 0
 
-while i < len(arr)-1:
-    while arr[i+1] >= arr[i]:
-        i += 1
-    left = arr[i]
-    j = i+1
-    valley_total = 0
-    while arr[j] < left and j < len(arr):
-        valley_total += (left - arr[j])
-        j += 1
-    if j != len(arr):
-        total_candy += valley_total
-    i = j
 
+def get_slice_total(a):
+    i = 0
+    slice_total = 0
+    while a[i] < 0:
+        i += 1
+    left = i
+
+    j = len(a) - 1
+    while a[j] < 0:
+        j -= 1
+
+    right = j
+
+    if left == right:
+        return 0
+
+    for k in range(left+1, right):
+        if a[k] < 0:
+            slice_total += 1
+
+    return slice_total
+
+
+while any([x >= 0 for x in arr]):
+    print(arr, get_slice_total(arr))
+    total_candy += get_slice_total(arr)
+    arr = [x-1 for x in arr]
 
 print(total_candy)
